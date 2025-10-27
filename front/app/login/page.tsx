8d2ddee2a7f025411a9095d86a622b88f076ba00
2litrewt <buicnuwe0170@gmail.com>
2025-10-27 15:18:55 +0900

    axiosを絶対パスに固定

--- front/app/login/page.tsx
+++ front/app/login/page.tsx
@@ -22,7 +22,7 @@ export default function Login() {
 
     try {
       const res = await apiClient.post(
-        "/api/auth/sign_in",      // 正しいログインパス
+        "api/auth/sign_in",      // 正しいログインパス
         { email, password },  // ボディはここに渡す
         { withCredentials: true }
       );
--- front/lib/axios.ts
+++ front/lib/axios.ts
@@ -7,8 +7,8 @@ const apiClient = axios.create({
   baseURL: isServer
   ? (process.env.NODE_ENV === "development"
       ? "http://back:3000"
-      : "https://back-main.fly.dev")
-  : "",
+      : (process.env.NEXT_PUBLIC_API_URL || "https://back-main.fly.dev"))
+      : (process.env.NEXT_PUBLIC_API_URL || "https://back-main.fly.dev"),
   withCredentials: true,
   headers: {
     "Content-Type": "application/json",
